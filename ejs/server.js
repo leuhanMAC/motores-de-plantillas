3e19ac702184dbf40464e9bb0fa940eddc0191b2
Nahuel Clotet <nahuee2011@gmail.com>
2022-08-20 23:36:22 -0300

    Fix port on EJS

--- ejs/server.js
+++ ejs/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Container = require("./contenedor");
 
 const productFiles = new Container("productos.json");
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const router = express.Router();
